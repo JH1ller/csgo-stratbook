69b8520a48db02c1f90661de3c9ad9687d9f2119
Justin Hiller <justin.hiller.privat@googlemail.com>
2021-01-31 18:16:09 +0100

    chore: remove unused route

--- landingpage/nuxt.config.js
+++ landingpage/nuxt.config.js
@@ -125,6 +125,7 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
+  buildDir: 'build/',
   generate: {
     dir: path.resolve(__dirname, '../server/dist_landingpage'),
   },
--- landingpage/pages/index.vue
+++ landingpage/pages/index.vue
@@ -4,8 +4,16 @@
   </section>
 </template>
 
-<script>
-export default {};
+<script lang="ts">
+export default {
+  mounted() {
+    console.log(
+      `%cHey there, looks like you're a developer checking out stratbook! If you want to have a deeper look, here is a free beta key: %c66D7B36A836090EBB985`,
+      `color: #c3c3c3; background: #141418; border: 1px solid #9fd1ff; border-right: none; padding: 2px 8px;`,
+      `color: #41b883; background: #141418; border: 1px solid #41b883; padding: 2px 8px;`
+    );
+  },
+};
 </script>
 
 <style lang="scss">
