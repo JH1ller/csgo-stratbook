4fcea4187365cf03d32171d0d5668e5c07192e44
Justin Hiller <justin.hiller.privat@googlemail.com>
2021-02-12 18:27:55 +0100

    fix: content filter error

--- client/src/utils/extractTextFromHTML.ts
+++ client/src/utils/extractTextFromHTML.ts
@@ -2,6 +2,5 @@ export const extractTextFromHTML = (html: string) => {
   const span = document.createElement('span');
   span.innerHTML = html;
   const text = span.textContent ?? span.innerText;
-  document.removeChild(span);
   return text;
 };
