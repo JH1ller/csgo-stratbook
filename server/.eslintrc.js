9ff8e9559d0ee23ed4b923cbf6f171dee0433f45
Clemens Susenbeth <clemens@susenbeth.com>
2021-03-19 03:37:35 +0100

    added nestjs core modules + updated eslint backend configuration + added prettier + added vscode eslint auto bindings + stabilized package version

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,12 +1,124 @@
+const { resolve } = require('path');
+
 module.exports = {
   root: true,
   env: {
     node: true,
+    jest: true,
   },
-  extends: ['plugin:security/recommended'],
+
   parserOptions: {
+    parser: '@typescript-eslint/parser',
+
+    project: resolve(__dirname, './tsconfig.json'),
+    tsconfigRootDir: __dirname,
+
+    sourceType: 'module',
     ecmaVersion: 2020,
   },
-  rules: {},
-  plugins: ['security'],
+
+  plugins: [
+    // required to apply rules which need type information
+    '@typescript-eslint',
+
+    // https://github.com/typescript-eslint/typescript-eslint/issues/389#issuecomment-509292674
+    // Prettier has not been included as plugin to avoid performance impact
+    // add it as an extension for your IDE
+    'prettier',
+  ],
+
+  extends: [
+    // https://github.com/typescript-eslint/typescript-eslint/tree/master/packages/eslint-plugin#usage
+    // ESLint typescript rules
+    'plugin:@typescript-eslint/recommended',
+
+    // consider disabling this class of rules if linting takes too long
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+
+    'standard',
+
+
+    // https://github.com/prettier/eslint-config-prettier#installation
+    // usage with Prettier, provided by 'eslint-config-prettier'.
+    'prettier',
+  ],
+
+  rules: {
+    'prettier/prettier': ['warn', {}, { usePrettierrc: true }],  // Use our .prettierrc file as source
+
+    'max-len': ['warn', {
+      'code': 120,
+      'tabWidth': 2,
+      'ignoreUrls': true,
+    }],
+
+    // allow async-await
+    'generator-star-spacing': 'off',
+
+    // allow paren-less arrow functions
+    'arrow-parens': 'off',
+    'one-var': 'off',
+    'no-extra-semi': 'error',
+    'import/first': 'off',
+    'import/named': 'error',
+    'import/namespace': 'error',
+    'import/default': 'error',
+    'import/export': 'error',
+    'import/extensions': 'off',
+    'import/no-unresolved': 'off',
+    'import/no-extraneous-dependencies': 'off',
+
+    'prefer-promise-reject-errors': 'off',
+    'semi': 'off',
+    '@typescript-eslint/semi': ['error', 'never'],
+    'space-before-function-paren': 'off',
+    'no-void': 'off',
+    '@typescript-eslint/no-inferrable-types': 0,
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error'],
+
+    // empty constructors are often used by nestjs
+    'no-useless-constructor': 'off',
+
+    'space-before-function-paren': ['error', {
+      'anonymous': 'never',
+      'named': 'never',
+      'asyncArrow': 'always'
+    }],
+
+    '@typescript-eslint/member-delimiter-style': ['error', {
+      'multiline': {
+        'delimiter': 'none',
+        'requireLast': false
+      },
+      'singleline': {
+        'delimiter': 'comma',
+        'requireLast': false
+      }
+    }],
+
+    'comma-dangle': ['warn', {
+      'arrays': 'always-multiline',
+      'objects': 'always-multiline',
+      // don't require dangling commas on imports as this breaks loader.transform-quasar-imports.js
+      'imports': 'never',
+      'exports': 'always-multiline',
+      'functions': 'always-multiline'
+    }],
+
+
+
+    // TypeScript
+    'quotes': 'off',
+    '@typescript-eslint/quotes': ['error', 'single'],
+
+    // Turn it off because already in @typescript-eslint
+    'no-unsafe-assignment': 'off',
+
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
 };
---
+++ server/src/main.ts
@@ -0,0 +1,17 @@
+import chalk from 'chalk'
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule)
+  await app.listen(3000)
+
+  if (module.hot) {
+    module.hot.accept()
+    module.hot.dispose(() => {
+      app.close().catch((err) => console.log(chalk.red(`failed to shutdown service ${err as string}`)))
+    })
+  }
+}
+
+bootstrap().catch((err) => console.log(chalk.red(`failed to start service ${err as string}`)))
