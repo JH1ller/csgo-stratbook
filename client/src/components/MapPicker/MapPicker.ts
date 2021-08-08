433ba02505e8f54d306b6f4980ac83625774df19
Clemens Susenbeth <clemens@susenbeth.com>
2021-08-08 02:37:43 +0200

    Revert "Attempt 3"
    This reverts commit 301867b6feb61d91c33b1d02a1392d6ec4d283bc.

--- client/src/components/MapPicker/MapPicker.ts
+++ client/src/components/MapPicker/MapPicker.ts
@@ -62,11 +62,6 @@ export default class MapPicker extends Vue {
       name: 'Ancient',
       active: true,
     },
-    {
-      _id: MapID.Ancient,
-      name: 'Ancient',
-      active: true,
-    },
   ];
 
   @Emit()
