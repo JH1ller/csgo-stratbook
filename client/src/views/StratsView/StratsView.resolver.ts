46fd1e86a52a5e24d399ce059e4d6930d4dce59a
Justin H <justin.hiller.privat@googlemail.com>
2022-03-16 19:31:11 +0100

    refactor: optimize page resolvers

--- client/src/views/StratsView/StratsView.resolver.ts
+++ client/src/views/StratsView/StratsView.resolver.ts
@@ -4,16 +4,18 @@ import { authGuard } from '@/guards/auth.guard';
 import { teamGuard } from '@/guards/team.guard';
 
 export const stratsResolver: NavigationGuard = async (to, from, next) => {
-  await store.dispatch('auth/fetchProfile');
+  //await store.dispatch('auth/fetchProfile');
 
   const authGuardResult: boolean = authGuard(to, from, next);
   if (!authGuardResult) return;
   const teamGuardResult: boolean = await teamGuard(to, from, next);
   if (!teamGuardResult) return;
 
-  const stratResponse: Response = await store.dispatch('strat/fetchStrats');
-  const teamResponse: Response = await store.dispatch('team/fetchTeamInfo');
-  const utilityResponse: Response = await store.dispatch('utility/fetchUtilities');
+  const [stratResponse, teamResponse, utilityResponse] = await Promise.all([
+    store.dispatch('strat/fetchStrats'),
+    store.dispatch('team/fetchTeamInfo'),
+    store.dispatch('utility/fetchUtilities'),
+  ]);
 
   if (!stratResponse.success || !teamResponse.success || !utilityResponse.success) {
     next(false);
--- client/src/views/UtilityView/UtilityView.resolver.ts
+++ client/src/views/UtilityView/UtilityView.resolver.ts
@@ -4,13 +4,15 @@ import { authGuard } from '@/guards/auth.guard';
 import { teamGuard } from '@/guards/team.guard';
 
 export const utilityResolver: NavigationGuard = async (to, from, next) => {
-  await store.dispatch('auth/fetchProfile');
+  // await store.dispatch('auth/fetchProfile');
 
   const authGuardResult: boolean = authGuard(to, from, next);
   if (!authGuardResult) return;
   const teamGuardResult: boolean = await teamGuard(to, from, next);
   if (!teamGuardResult) return;
 
+  //* if we have no utilities in store already, only resolve if request is successful.
+  // TODO: probably could be handled better.
   if (store.state.utility.utilities.length) {
     store.dispatch('utility/fetchUtilities');
   } else {
