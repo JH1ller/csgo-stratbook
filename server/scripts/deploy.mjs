4bb2b3df3b6025e7437003e333b7767df5fe48c9
Justin H <justin.hiller.privat@googlemail.com>
2022-03-30 19:37:55 +0200

    chore: rebuild fe

--- server/scripts/deploy.mjs
+++ server/scripts/deploy.mjs
@@ -3,12 +3,13 @@
 cd('../client');
 
 await $`npm run build:staging`;
-await $`git add .`;
-await $`git commit -m "chore: rebuild fe" || echo "No changes to commit"`;
 
 cd('..');
 
-await $`git subtree push --prefix server heroku master`;
+await $`git add .`;
+await $`git commit -m "chore: rebuild fe" || echo "No changes to commit"`;
+
+//await $`git subtree push --prefix server heroku master`;
 //await $`git push heroku \`git subtree split --prefix server master\`:master --force`;
 
 console.log('done!');
