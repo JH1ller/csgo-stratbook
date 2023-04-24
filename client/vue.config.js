0e95070250103bc22a93a7c09994dd24ff13fd95
JH1ller <justin.hiller.privat@googlemail.com>
2023-04-24 11:24:35 +0200

    chore: update dependencies

--- client/vue.config.js
+++ client/vue.config.js
@@ -33,6 +33,21 @@ module.exports = {
       config.target = 'electron-renderer';
     }
     config.devtool = process.env.NODE_ENV !== 'production' ? 'eval-source-map' : false;
+    config.resolve = {
+      ...config.resolve,
+      fallback: {
+        fs: false,
+        tls: false,
+        net: false,
+        path: false,
+        zlib: false,
+        http: false,
+        https: false,
+        stream: false,
+        crypto: false,
+        os: false,
+      },
+    };
   },
   transpileDependencies: ['replace-keywords'],
   pluginOptions: {
