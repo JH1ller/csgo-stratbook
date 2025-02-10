36bfaeee3e266748b9c98c89ebae3e6c3a9da9d6
JH1ller <justin.hiller.privat@googlemail.com>
2025-02-10 22:54:32 +0100

    feat: favicon, util item quick filters

--- client/src/components/UtilityItem/UtilityItem.vue
+++ client/src/components/UtilityItem/UtilityItem.vue
@@ -10,11 +10,11 @@
     <div v-else class="utility-item__image utility-item__copy-wrapper">
       <fa-icon icon="expand" class="utility-item__icon-copy" />
     </div>
-    <div class="utility-item__side-wrapper">
+    <div class="utility-item__side-wrapper" @click="filterSide">
       <img v-if="utility.side === Sides.T" src="@/assets/icons/t_badge.png" class="utility-item__side" />
       <img v-else src="@/assets/icons/ct_badge.png" class="utility-item__side" />
     </div>
-    <UtilityTypeDisplay :type="utility.type" class="utility-item__type" />
+    <UtilityTypeDisplay :type="utility.type" class="utility-item__type" @click.native.stop="filterType" />
     <span class="utility-item__info">
       <div class="utility-item__name">{{ utility.name }}</div>
       <button v-if="!readOnly" class="utility-item__context" @click="openMenu"><fa-icon icon="caret-down" /></button>
