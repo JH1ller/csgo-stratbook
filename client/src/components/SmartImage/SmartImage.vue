33bdbb19c30a1ac5f95e97a352bcda7d1233b729
Justin Hiller <justin.hiller@engelvoelkers.com>
2025-02-18 16:50:59 +0100

    feat: revert change to smartimage alt

--- client/src/components/SmartImage/SmartImage.vue
+++ client/src/components/SmartImage/SmartImage.vue
@@ -2,8 +2,7 @@
   <div class="smart-image" :class="state">
     <img :src="src" :alt="alt" @load="load" @error="error" class="smart-image__img" draggable="false" />
     <Loader class="smart-image__loader" />
-    <p v-if="alt" class="smart-image__alt">{{ alt }}</p>
-    <fa-icon v-else icon="exclamation-triangle" class="smart-image__error" />
+    <fa-icon icon="exclamation-triangle" class="smart-image__error" />
   </div>
 </template>
 
